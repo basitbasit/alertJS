b898da269c8a9150d767416f54175379b5a55df5
basitbasit <basitbasit4060@gmail.com>
2023-09-27 00:02:51 +0500

    alertJS

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("Error enter the valid password")
+alert("Welcome To JS land Happy Codding")
+alert("Welcome To JS land ")
+alert("prevent this page from creating addtional dilogs")
+alert("hello--!  can JS run through my web browser's consale")
+var pizza = "pizza\npizz\npiz\npi\np"
+
+
+alert(myName)
+alert(pizza)
